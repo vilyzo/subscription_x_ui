08d0291bb63bac94d441f4cfbdfb2442fd6b94c7
Vilyzo <vilyzo@example.com>
2024-12-04 22:17:59 +1200

    First

---
+++ app.py
@@ -0,0 +1,43 @@
+import logging
+from starlette.responses import PlainTextResponse
+from base64encoder import Base64Encoder
+import config
+from database import Database
+from fastapi import FastAPI, HTTPException
+
+logging.basicConfig(level=logging.INFO)
+file_handler = logging.FileHandler('app.log', mode='a')
+file_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger = logging.getLogger()
+logger.addHandler(file_handler)
+
+SERVER_ADDRESS = config.SERVER_ADDRESS
+PORT = config.SERVER_PORT
+DB_PATCH = config.DB_PATCH
+PUBLIC_KEY = config.PUBLIC_KEY
+FP = config.FP
+SNI = config.SNI
+SID = config.SID
+
+
+app = FastAPI()
+db = Database(DB_PATCH)
+encoder = Base64Encoder()
+
+
+@app.get("/sub/{sub_id}", response_class=PlainTextResponse)
+async def send_sub(sub_id: str):
+    # sub_id = sub_id
+    await db.connect()
+    client_data = await db.get_data_by_sub_id(sub_id)
+    await db.close()
+    if client_data is None:
+        raise HTTPException(status_code=404, detail="Subscription not found")
+    uuid = client_data["uuid"]
+    email = client_data["email"]
+    key = f'vless://{uuid}@{SERVER_ADDRESS}:{PORT}?type=tcp&security=reality&pbk={PUBLIC_KEY}&fp={FP}&sni={SNI}&sid=9b534a13&spx=%2F&flow=xtls-rprx-vision#{email}'
+    # key = encoder.encode(key)
+
+    return encoder.encode(key)
---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+DB_PATCH = os.getenv("DB_PATCH", "clients.db")
+SERVER_ADDRESS = os.getenv("SERVER_ADDRESS", "localhost")
+SERVER_PORT = os.getenv("SERVER_PORT", 8000)
+PUBLIC_KEY = os.getenv("PUBLIC_KEY", "BS5Yv5xIQ8aGlUmJRaYxye9f_yfrv-Q348tAPmJdaFs")
+SID = os.getenv("SID", "06d31f198f3dd6")
+SNI = os.getenv("SNI", "google.com")
+FP = os.getenv("FP", "safari")
