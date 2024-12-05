ccdfb212bc78ddb53e11c1a90b3e217114ed05ef
Vilyzo <vilyzo@example.com>
2024-12-05 14:29:23 +1200

    Style

--- install_with_docker_compose.sh
+++ install_with_docker_compose.sh
@@ -21,8 +21,8 @@ ask_variable() {
 
 # Клонируем репозиторий
 echo "Клонирование репозитория..."
-git clone https://github.com/vilyzo/subscription_x_ui.git .
 cd subscription_x_ui
+git clone https://github.com/vilyzo/subscription_x_ui.git .
 # Устанавливаем Docker
 echo "Установка Docker..."
 sudo apt update
